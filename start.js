09aa251aa92c6521b83af263efe1eb99ded5b886
宇天 <yutent@doui.cc>
2016-08-31 21:21:25 +0800

    优化模板配置;优化async;

--- start.js
+++ start.js
@@ -11,7 +11,8 @@ let app = new (require('dojs'))(__dirname + '/')
 let db = require(BASE + 'config/db.json')
 
 app.use('website', 'dojs.cc') //设置网址
-app.use('domain', 'dojs.cc') //设置域，cookie用到，默认等于website
+app.use('session.type', 'redis') //设置session的存储类型，redis/native
+app.use('session.domain', 'dojs.cc') //设置session域，cookie用到，默认等于website
 app.use('port', 3000) //设置端口,默认3000
 
 for(let i in db){
